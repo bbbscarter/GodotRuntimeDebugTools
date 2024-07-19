3773a84cada5e3016c94d52b7575a10492e287f4
Simon Carter <bbbscarter@gmail.com>
2024-07-19 20:25:52 +0100

    Prep for submission to Asset Lib

--- addons/runtime_debug_tools/scripts/plugin.gd
+++ addons/runtime_debug_tools/scripts/plugin.gd
@@ -9,8 +9,8 @@ func _enter_tree():
     add_debugger_plugin(_editor_plugin)
     EditorInterface.get_inspector().edited_object_changed.connect(_editor_plugin.selection_changed)
 
-    if not ProjectSettings.has_setting("autoload/RemoteInspector"):
-        add_autoload_singleton("RemoteInspector", _runtime_scene)
+    if not ProjectSettings.has_setting("autoload/RuntimeDebugTools"):
+        add_autoload_singleton("RuntimeDebugTools", _runtime_scene)
 
     _editor_ui_scene.set_remote_inspector(_editor_plugin)
     add_control_to_container(CONTAINER_TOOLBAR, _editor_ui_scene)
@@ -19,7 +19,7 @@ func _exit_tree():
     if _editor_plugin != null:
         EditorInterface.get_inspector().edited_object_changed.disconnect(_editor_plugin.selection_changed)
         remove_debugger_plugin(_editor_plugin)
-    if ProjectSettings.has_setting("autoload/RemoteInspector"):
-        remove_autoload_singleton("RemoteInspector")
+    if ProjectSettings.has_setting("autoload/RuntimeDebugTools"):
+        remove_autoload_singleton("RuntimeDebugTools")
 
     remove_control_from_container(CONTAINER_TOOLBAR, _editor_ui_scene)
